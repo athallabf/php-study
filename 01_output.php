9b01aa11efa7e09f4d861a374ea13ee14e534874
AthallaITS <5027231018@student.its.ac.id>
2024-01-30 20:09:55 +0700

    add 01 file

---
+++ 01_output.php
@@ -0,0 +1,14 @@
+<!-- <?php
+
+// echo
+// echo 'ngop', 123, 10.4 ;
+
+// print_r([1,2,3]);
+
+// printf('sng\n');
+
+// returns more info
+var_dump('hellp');
+
+
+var_export('Hello') ; // 'Hello' with quotes -->
