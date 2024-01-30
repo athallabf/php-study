9d388f42a3a1cae0aada2e9c5b6d7190c66e48ec
AthallaITS <5027231018@student.its.ac.id>
2024-01-30 20:11:21 +0700

    add 05 file

---
+++ 05_loops.php
@@ -0,0 +1,64 @@
+/<?php
+
+  /* -------- Loops & Iteration ------- */
+
+  /* ------------ For Loop ------------ */
+
+  /*
+** For Loop Syntax
+  for (initialize; condition; increment) {
+  // code to be executed
+  }
+*/
+
+
+  /* ------------ While Loop ------------ */
+
+  /*
+** While Loop Syntax
+  while (condition) {
+  // code to be executed
+  }
+*/
+
+
+  /* ---------- Do While Loop --------- */
+
+  /*
+** Do While Loop Syntax
+  do {
+  // code to be executed
+  } while (condition);
+
+do...while loop will always execute the block of code once, even if the condition is false.
+*/
+
+
+
+  /* ---------- Foreach Loop ---------- */
+
+  /*
+** Foreach Loop Syntax
+  foreach ($array as $value) {
+  // code to be executed
+  }
+// */
+
+  $ngops = $benar[0] ?? 'gaada';
+  echo $ngops;
+
+  // $arr = [1, 2, 3, 4, 5];
+
+  // mirip for of
+  // foreach ($arr as $index => $num) {
+  // / echo $index . '--' . $num . '<br>';
+
+  $keyvalue = [
+    'satu' => 1,
+    'dua' => 2,
+    'tiga' => 3
+  ];
+
+// foreach ($keyvalue as $key => $value) {
+// echo $key . '=>' . $value;
+// }
