09ab344f18782b9e9258a3dd40d1d2b4bc77cf52
AthallaITS <5027231018@student.its.ac.id>
2024-01-30 20:11:05 +0700

    add 04 file

---
+++ 04_conditionals.php
@@ -0,0 +1,59 @@
+<?php
+
+/* ---- Conditionals & Operators ---- */
+
+/* ------------ Operators ----------- */
+
+/*
+< Less than
+> Greater than
+<= Less than or equal to
+>= Greater than or equal to
+== Equal to
+=== Identical to
+!= Not equal to
+!== Not identical to
+ */
+
+/* ---------- If & If-Else Statements --------- */
+
+/*
+ ** If Statement Syntax
+if (condition) {
+// code to be executed if condition is true
+}
+ */
+
+/* -------- Ternary Operator -------- */
+/*
+The ternary operator is a shorthand if statement.
+Ternary Syntax:
+condition ? true : false;
+ */
+
+/* -------- Switch Statements ------- */
+
+// $age = 20;
+// // if($age > 17) echo 'ngops';
+// if ($age == 17) {
+//     echo 'ngops';
+// } else if ($age > 19) {
+//     echo '19';
+// }
+
+// $time = date("H");
+// if ($time > 5 && $time < 10) {
+//     echo "$time";
+// } else if ($time > 10) {
+//     echo "$time ngops";
+// }
+
+// $benar = [];
+// if ($benar) {
+//     echo 'empty array';
+// }
+
+// bisa juga pake empty()
+// if (empty($benar)) {
+//     echo 'd';
+// }
